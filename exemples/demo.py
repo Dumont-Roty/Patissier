7769dab806b020bf28515375e507af1c84d3d3d7
Pierre DUMONT ROTY <pierre-dr@hotmail.com>
2025-03-26 14:48:14 +0100

    Planing Fraise et Choux

--- exemples/demo.py
+++ exemples/demo.py
@@ -1,23 +1,27 @@
 from patissier.recettes import *
+from graphlib import *
 
 # Création d'une recette
 ListeNomsSousProduits = ["Fraises coupées", "Pâte à tarte"," Pâte à chou", "Pâte à chou reposée", "Choux vides", "Choux à la crème",
- "Crème pâtissière", "Pâte à tarte cuite","Pâte à tarte cuite et froide","Tartes aux fraises"]
+ "Crème patissière", "Pâte à tarte cuite","Pâte à tarte cuite et froide","Tartes aux fraises"]
 
 ListeSousProduits = [sousProduit(nom) for nom in ListeNomsSousProduits]
 
-ListeEtapes = [
+EtapesTarteAuxFraises = [
     etape("Couper les fraises", produitFinal=sousProduit("Fraises coupées"), duree=10),
-    etape("Préparer crème patissière", produitFinal=sousProduit("Crème pâtissière"), duree=30),
+    etape("Préparer crème patissière", produitFinal=sousProduit("Crème patissière"), duree=30),
     etape("Préparer la pâte à tarte", produitFinal=sousProduit("Pâte à tarte"), duree=5),
-    etape("Préparer pâte à chou", produitFinal=sousProduit("Pâte à chou"), duree=15),
     etape("Cuisson pâte à tarte", produitFinal=sousProduit("Pâte à tarte cuite"), duree=45,
           besoinFour=True, etapeActive=False, sousProduitNecessaires=[sousProduit("Pâte à tarte")]),
     etape("Attendre la pâte froide", produitFinal=sousProduit("Pâte à tarte cuite et froide"), duree=30,
           etapeActive=False, sousProduitNecessaires=[sousProduit("Pâte à tarte cuite")]),
     etape("Dressage tarte aux fraises", produitFinal=sousProduit("Tartes aux fraises"), duree=10,
-          etapeActive=True, sousProduitNecessaires=[sousProduit("Pâte à tarte cuite et froide"), sousProduit("Fraises coupées"), sousProduit("Crème pâtissière")]),
+          etapeActive=True, sousProduitNecessaires=[sousProduit("Pâte à tarte cuite et froide"), sousProduit("Fraises coupées"), sousProduit("Crème patissière")]),
+]
 
+EtapesChoux = [
+    etape("Préparer crème patissière", produitFinal=sousProduit("Crème patissière"), duree=30),
+    etape("Préparer pâte à chou", produitFinal=sousProduit("Pâte à chou"), duree=15),
     etape("Faire reposer pâte chou", produitFinal=sousProduit("Pâte à chou reposée"), duree = 60,
                     etapeActive = False, sousProduitNecessaires=[sousProduit("Pâte à chou")]),
     etape("Cuisson choux", produitFinal=sousProduit("Choux vides"), duree = 25,
@@ -25,3 +29,10 @@ ListeEtapes = [
     etape("Dressage choux", produitFinal=sousProduit("Choux à la crème"), duree = 20,
                     sousProduitNecessaires=[sousProduit("Choux vides"), sousProduit("Crème patissière")])
 ]
+
+RecetteTarteAuxFraises = recette(EtapesTarteAuxFraises)
+RecetteChoux = recette(EtapesChoux)
+
+MonPlanning = planing([RecetteChoux,RecetteTarteAuxFraises])
+MonPlanning.genererPlaning()
+print(MonPlanning)
--- src/patissier/recettes.py
+++ src/patissier/recettes.py
@@ -1,10 +1,16 @@
 from typing import List
+from graphlib import *
 
 class sousProduit(object):
 
     def __init__(self, nom :str):
         self.nom : str = nom
 
+    def __eq__(self, autre):
+        return self.nom == autre.nom
+    
+    def __hash__(self):
+        return hash(self.nom)
 
 class etape(object):
     # si peut de chose dans le init alors plus simple
@@ -18,4 +24,79 @@ class etape(object):
         self.active : bool = etapeActive
         self.sousProduitFinal : sousProduit = produitFinal
         self.sousProduitNecessaires : List[sousProduit] = sousProduitNecessaires
-
+        
+    def __eq__(self, autre):
+        if self.sousProduitFinal == autre.sousProduitFinal and self.sousProduitNecessaires == autre.sousProduitNecessaires:
+            return True
+        return False
+                
+        
+class recette(object):
+    def __init__(self, listeEtapes : list[etape]):
+        self.listeEtapes : list[etape] = listeEtapes
+        
+        self.listesousProduits : list[sousProduit] = []
+        for step in self.listeEtapes:
+            if step.sousProduitFinal not in self.listesousProduits:
+                self.listesousProduits.append(step.sousProduitFinal)
+                
+    def verifierValide(self):
+        """_summary_
+        Vérifie si les sous produits necessaires pour la recette peuve,t etre obtnue via une étape de la recette.
+        
+        Paremètre :
+        - Aucun
+        
+        Renvoie : 
+        - booléen : Vrai si la recette est auto-suffisante, faux sinon
+        """
+        listePrerequis = []
+        for step in self.listeEtapes:
+            for prerequis in step.sousProduitNecessaires:
+                if prerequis not in self.listesousProduits:
+                    return False
+        return True 
+    
+    
+class planing(object):
+    
+    def __init__(self, listeRecette : str):
+        self.listeRecette : list[recette] = listeRecette
+        
+        self.listeSousProduit = []
+        for recipe in self.listeRecette:
+            self.listeSousProduit += [ prod for prod in recipe.listesousProduits if prod not in self.listeSousProduit ]
+        
+        self.listeEtape = []
+        for recipe in self.listeRecette:
+            self.listeEtape += [ step for step in recipe.listeEtapes if step not in self.listeEtape]
+            
+        self.planning : List[etape] = []
+            
+    def genererPlaning(self):
+        
+        graph =  dict()
+        for sousProduit in self.listeSousProduit:
+            L = []
+            for step in self.listeEtape:
+                    if( step.sousProduitFinal == sousProduit ):
+                        L += [ produits for produits in step.sousProduitNecessaires if produits not in L ]
+                        # Ajout de l'intégralité des prédécésseur d'un sous produit sans avoir de doublon # CONCATENE les listes
+                        # L.append(step.sousProduitNecessaires) ===> [1,2,[3,4]]
+            graph[sousProduit] = list(L) #Obtention des prédécésseur d'un sous produit en créant une copie de la liste
+        ts = TopologicalSorter(graph)
+        triTopo = list(ts.static_order())
+        self.planning = []
+        for prod in triTopo:
+            for step in self.listeEtape:
+                if step.sousProduitFinal == prod:
+                    self.planning.append(step)
+            
+    def __str__(self):
+        S = "Ordre possible : \n"
+        temps = 0
+        for i, step in enumerate(self.planning):
+            S += str(i+1) + ") " + step.nom + ", durée : " + str(step.duree) + " min\n"
+            temps += step.duree
+        S += "Temps total : " + str(temps) + " min"
+        return S 
